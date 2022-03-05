89ab6d6defb66dd025a73f4d83bc8a9cd2d86b74
EmanAbobakr <43084491+EmanAbobakr@users.noreply.github.com>
2022-03-05 10:04:08 +0200

    server ui

--- src/tictactoeserver/FXMLDocumentController.java
+++ src/tictactoeserver/FXMLDocumentController.java
@@ -7,9 +7,12 @@ package tictactoeserver;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
 
 /**
@@ -19,17 +22,25 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private PieChart pieChartId;
+
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("Grapefruit", 13),
+                new PieChart.Data("Oranges", 25),
+                new PieChart.Data("Plums", 10),
+                new PieChart.Data("Pears", 22),
+                new PieChart.Data("Apples", 30));
+        pieChartId.setData(pieChartData);
+        
+        final PieChart pieChartId = new PieChart(pieChartData);
+        
     }    
     
 }
